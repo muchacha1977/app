f8785089a262e9ecf0882a44bd5bc87647fcf415
muchacha1977 <maryna.varavina@googlemail.com>
2014-10-22 00:03:03 +0200

    Recap
    including data-binding, controllers, services, and routin

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,26 @@
 /* App Module */
 
 angular.module('myApp',['ui.router'])
+    .config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        function($stateProvider,$urlRouterProvider){
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                })
+                .state('posts',{
+                     url:'/posts/{id}',
+                     templateUrl:'/posts.html',
+                     controller:'PostsCtrl'
+                    });
+
+
+            $urlRouterProvider.otherwise('home');
+        }])
+
     .factory('myPosts', [function(){
         var o={
             posts:[
@@ -15,6 +35,11 @@ angular.module('myApp',['ui.router'])
 
         return o;
     }])
+    .controller('PostsCtrl',['$scope','$stateParams',
+        'myPosts',
+        function($scope, $stateParams, myPosts){
+            $scope.post = myPosts.posts[$stateParams.id];
+        }])
     .controller('MainCtrl',[
         '$scope',
         'myPosts',
@@ -26,7 +51,11 @@ angular.module('myApp',['ui.router'])
                 $scope.posts.push({
                     title: $scope.title,
                     link: $scope.link,
-                    upvotes: 0
+                    upvotes: 0,
+                    comments: [
+                        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                    ]
                 });
                 $scope.title = '';
                 $scope.link = '';
